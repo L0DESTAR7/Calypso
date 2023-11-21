0ac6261949825e3d0509182843870b6ae89f5e45
Lodestar7 <godhand2006@hotmail.fr>
2023-11-21 07:05:11 +0100

    style: 💄 Improved ForecastContainer Layout

--- components/ForecastContainer.tsx
+++ components/ForecastContainer.tsx
@@ -12,9 +12,9 @@ export default function ForecastContainer(props: ForecastContainerProps) {
 
 
   return (
-    <div className='grid grid-cols-1 content-center w-28 h-48 border-solid rounded-3xl border-4 border-bunker-300/[.66] relative'>
-      <div className={`flex flex-col space-y-6 place-items-center text-center ${jura.className} font-bold capitalize`}>
-        <div className='text-xl tex-bunker-950 place-self-center absolute top-2' suppressHydrationWarning={true}>{props.day}</div>
+    <div className='grid grid-cols-1 content-center w-28 [&>*]:h-48 border-solid rounded-3xl border-4 border-bunker-300/[.66]'>
+      <div className={`grid grid-row-3 content-around place-items-center text-center rounded-3xl ${jura.className} font-bold capitalize`}>
+        <div className='text-xl tex-bunker-950 place-self-center' suppressHydrationWarning={true}>{props.day}</div>
         <div className='text-3xl text-bunker-950' suppressHydrationWarning={true}>{`${props.temp}°`}</div>
         <div className='text-xl text-bunker-300' suppressHydrationWarning={true}>{getWeatherDescription(props.condition)}</div>
       </div>
